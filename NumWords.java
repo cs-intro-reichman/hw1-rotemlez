77ae627a50a709f62ee8968bf6368e4373c39f9b
rotemlez <rotem.lezerovich@post.runi.ac.il>
2025-11-05 14:20:25 +0200

    completed assignment

--- NumWords.java
+++ NumWords.java
@@ -2,9 +2,11 @@
 public class NumWords {
 	public static void main(String args[]) {
 	int num = Integer.parseInt(args[0]);
+
         int hundreds = num / 100;
         int tens = (num % 100) / 10; 
         int ones = num % 10;
-        System.out.println(hundreds + " hundreds, "+ tens + " tens and " + ones + " ones");
+
+        System.out.print(hundreds + " hundreds, "+ tens + " tens, " + ones + " ones");
 	}
 }
